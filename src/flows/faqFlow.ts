fa5d63a8861480e4098dd2d2c599c6d86b281763
Ramon <racolinadevelop@gmail.com>
2025-10-22 00:34:33 -0400

    omg

--- src/flows/faqFlow.ts
+++ src/flows/faqFlow.ts
@@ -5,7 +5,7 @@ import chatGPT from "~/services/chatGPT";
 import spreadSheets from "~/services/spreadSheets";
 import "dotenv/config";
 
-const pathPrompt = path.join(process.cwd(), "assets", "prompts", "prompt_ChatGPT.txt");
+const pathPrompt = path.join(process.cwd(), "assets", "prompts", "prompt_chatGPT.txt");
 const prompt = fs.readFileSync(pathPrompt, "utf-8");
 
 export const faqFlow = addKeyword(EVENTS.ACTION)
