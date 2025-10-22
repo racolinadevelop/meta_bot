230b973c1cd72846c8ae9c9102ae8fb8bed26fee
Ramon <racolinadevelop@gmail.com>
2025-10-21 23:36:29 -0400

    fix promps

--- src/flows/faqFlow.ts
+++ src/flows/faqFlow.ts
@@ -7,7 +7,7 @@ import "dotenv/config";
 
 const pathPrompt = path.join(
     process.cwd(),
-    "assets/Prompts",
+    "assets/prompts",
     "prompt_ChatGPT.txt"
 );
 const prompt = fs.readFileSync(pathPrompt, "utf-8");
